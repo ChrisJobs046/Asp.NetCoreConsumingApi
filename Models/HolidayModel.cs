f726200126963783087a3ce8ac0b5129b466cd3e
ChrisJobs046 <ChrisJobs046@gmail.com>
2021-06-08 11:12:47 -0400

    Modelo de datos creado

---
+++ Models/HolidayModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Asp.NetCoreConsumingApi.Models
+{
+    public class HolidayModel
+    {
+        public string Name { get; set; }
+        public string LocalName { get; set; }
+        public DateTime? Date { get; set; }
+        public string CountryCode { get; set; }
+        public bool Global { get; set; }
+    }
+}
